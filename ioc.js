30a4909c13c572fe81cb622f6625e60166bf51b5
Barry O Sullivan <barry@tercet.io>
2016-02-01 12:11:19 +0000

    IOC now includes try catch functionality and is no longer ducktyped.

--- ioc.js
+++ ioc.js
@@ -17,22 +17,25 @@ var ioc = {
     }
 
     //It's an interface
-    var concreteClass = this.interfaceToConcrete[interface._guid];
+    var concreteClass = this.interfaceToConcrete[interface._iid];
     if (!concreteClass) {
-      throw "Could not load a concrete class for interface["+interface._guid+"]: " + interface;
+      throw "Could not load a concrete class for interface["+interface._iid+"]: " + interface;
     }
     return concreteClass;
   },
   
-  bind: function (interface, object) {
-    var concrete = object;
-    //If it's a constructor, make it
-    if (typeof object === 'function') {
-      concrete = this.make(object);
-    }
+  bind: function (interface, classRef) {
+    var concrete = classRef;
 
-    this.classFactory.assertObjectImplementsInterface(concrete, interface);
-    this.interfaceToConcrete[interface._guid] = concrete;
+    if (typeof classRef === 'function') {
+      concrete = this.make(classRef);
+    }
+    
+    if (concrete._implements.indexOf(interface._iid) === -1) {
+      throw "Binding error: Bound class does not implement the interface";
+    }
+    
+    this.interfaceToConcrete[interface._iid] = concrete;
   },
   
   enableInterfaceChecking: function() {
@@ -41,21 +44,43 @@ var ioc = {
   
   disableInterfaceChecking: function() {
     this.isInterfaceCheckingOn = false;
+  },
+  
+  try: function(callback) {
+    try{
+      callback();
+    } catch (error) {
+      return {
+        catch: function(interfaceOrClass, catchCallback) {
+          if ((interfaceOrClass.prototype && interfaceOrClass.prototype._cid && error._extends.indexOf(interfaceOrClass.prototype._cid) !== -1)  
+            || (interfaceOrClass._iid && error._implements.indexOf(interfaceOrClass._iid) !== -1)) {
+            catchCallback(error);
+            return {catch: function(){return this;}, throwUncaught: function(){}};
+          }
+          return this;
+        },
+        throwUncaught: function() {
+          throw error;
+        }
+      };
+    } 
   }
 
 };
 
 ioc.interfaceFactory = {
-  _guid: 1,
+  _iid: 1,
   make: function (schema) {
-    schema._guid = this._guid;
-    ++this._guid;
+    schema._iid = this._iid;
+    ++this._iid;
     if (!ioc.isInterfaceCheckingOn) {
       return schema;
     }
     var interfaces = schema._extends;
+    schema._extends = [schema._iid];
     for (var key in interfaces) {
       schema = this.extend(schema, interfaces[key]);
+      schema._extends = schema._extends.concat(interfaces[key]._extends);
     }
     return schema;
   },
@@ -72,13 +97,21 @@ ioc.interfaceFactory = {
 
 //Factory for making classes that implement interfaces
 ioc.classFactory = {
+  _cid: 1,
   make: function (schema) {
     var baseClass = schema._extends || Stapes;
+    delete(schema._extends);
     var constructor = baseClass.subclass(schema, true);
+    
+    constructor.prototype._cid = this._cid;
+    ++this._cid;
+    constructor.prototype._extends = [constructor.prototype._cid].concat(constructor.prototype._extends || []);
+    
     var interfaces = schema._implements;
-    constructor.prototype._implements = interfaces;
+    constructor.prototype._implements = [];
     for (var key in interfaces) {
       this.assertObjectImplementsInterface(schema, interfaces[key]);
+      constructor.prototype._implements = constructor.prototype._implements.concat(interfaces[key]._extends);
     }
     return constructor;
   },
